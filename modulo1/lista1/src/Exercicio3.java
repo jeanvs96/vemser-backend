d9176e45f43859a3aafed85ddfe14947fdc7d58e
Jean <jean.silva@dbccompany.com.br>
2022-06-17 21:53:06 -0300

    lista1: exercicio 3

---
+++ modulo1/lista1/src/Exercicio3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        double valorConsumido, valorPago;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Valor consumido:");
+        valorConsumido = scanner.nextDouble();
+        System.out.println("Valor pago:");
+        valorPago = scanner.nextDouble();
+        scanner.close();
+
+        if (valorPago < valorConsumido) {
+            System.out.println("O valor pago deve ser maior ou igual ao valor consumido.");
+        } else if (valorPago >= valorConsumido) {
+            System.out.println("Troco: \nR$" + (valorPago - valorConsumido));
+        }
+    }
+}
