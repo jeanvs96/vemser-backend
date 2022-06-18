2225ca9a6de6fe515b649c0e2d81d989f9ce489e
Jean <jean.silva@dbccompany.com.br>
2022-06-17 23:04:51 -0300

    lista1: exercicio 7

---
+++ modulo1/lista1/src/Exercicio7.java
@@ -0,0 +1,10 @@
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Integer valor1 = 10, valor2 = 20, aux;
+        aux = valor1;
+        valor1 = valor2;
+        valor2 = aux;
+
+        System.out.println("Valor 1: " + valor1 + "\nValor 2: " + valor2);
+    }
+}
