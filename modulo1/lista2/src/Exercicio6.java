d04fa2ed8d06c95e78bdaa51f692e3d46462d317
Jean <jean.silva@dbccompany.com.br>
2022-06-20 22:32:15 -0300

    lista2: exercicio 6

---
+++ modulo1/lista2/src/Exercicio6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer [] numeros = {1, 2, 3, 1, 4, 6, 7, 4, 4, 1};
+        Integer numeroParaConferir;
+        boolean numeroEncontrado = false;
+
+        System.out.println("Informe o número para conferir se existe no vetor: ");
+        numeroParaConferir = scanner.nextInt();
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i].equals(numeroParaConferir)) {
+                System.out.println("Número encontrado na posição: " + i + " do vetor 'numeros'");
+                numeroEncontrado = true;
+            }
+        }
+        if (!numeroEncontrado) {
+            System.out.println("O número informado não existe no vetor.");
+        }
+
+    }
+}
