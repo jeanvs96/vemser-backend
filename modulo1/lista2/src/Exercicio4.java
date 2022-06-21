460f6fe4c5d4d1b21ddf2bcd01cd65fbb1da4dc7
Jean <jean.silva@dbccompany.com.br>
2022-06-20 21:49:29 -0300

    lista2: exercicio 4

---
+++ modulo1/lista2/src/Exercicio4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] numeros = new int [3];
+        int posicaoDoMenor = 0, controle = 999999999;
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Informe o " + (i + 1) +"° valor: ");
+            numeros[i] = scanner.nextInt();
+            if(numeros[i] < controle) {
+                posicaoDoMenor = i;
+                controle = numeros[i];
+            }
+        }
+        System.out.println("A posição do menor valor é: " + posicaoDoMenor);
+    }
+}
