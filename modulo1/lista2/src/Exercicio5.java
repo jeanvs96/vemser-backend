f305ce27bc712a242a96a288361ed6b3073041e0
Jean <jean.silva@dbccompany.com.br>
2022-06-20 21:58:03 -0300

    lista2: exercicio 5

---
+++ modulo1/lista2/src/Exercicio5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] valores = new int[20];
+
+        for (int i = 0; i < valores.length; i++) {
+            System.out.println("Informe o " + (i + 1) + "° valor");
+            valores[i] = scanner.nextInt();
+        }
+        System.out.println();
+        for (int i = valores.length - 1; i >= 0; i--) {
+            System.out.print(valores[i] + " ");
+        }
+    }
+}
