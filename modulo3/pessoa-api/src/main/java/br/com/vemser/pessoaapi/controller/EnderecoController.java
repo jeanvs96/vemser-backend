6901945300ff1795f2871821488ff255d323cef1
Jean <jean.silva@dbccompany.com.br>
2022-07-08 20:55:26 -0300

    modulo3: aula 5 fix homework 5

--- modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/controller/EnderecoController.java
+++ modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/controller/EnderecoController.java
@@ -2,7 +2,6 @@ package br.com.vemser.pessoaapi.controller;
 
 import br.com.vemser.pessoaapi.dto.EnderecoCreateDTO;
 import br.com.vemser.pessoaapi.dto.EnderecoDTO;
-import br.com.vemser.pessoaapi.entity.Endereco;
 import br.com.vemser.pessoaapi.exceptions.RegraDeNegocioException;
 import br.com.vemser.pessoaapi.service.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
